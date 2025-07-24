6b8ed448e02993af2ce574724bffdf6cd280ae2c
michael <michael.zakhary33@gmail.com>
2025-07-24 18:45:07 -0400

    finished the project and made it compatible with any book in the books directory

---
+++ main.py
@@ -0,0 +1,27 @@
+from stats import get_book_text
+from stats import count_words
+from stats import letter_stats
+from stats import sort_dictionary
+from stats import final_report
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python3 main.py <path_to_book>")
+    
+    book_path = sys.argv[1]
+
+    text = get_book_text(book_path)
+    count = count_words(book_path)
+    letters = letter_stats(text)
+    sort = sort_dictionary(letters)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+    print(final_report(sort))
+    print("============= END ===============")
+
+main()
---
+++ stats.py
@@ -0,0 +1,32 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def count_words(contents):
+    book = get_book_text(contents)
+    return f"{len(book.split())}"
+
+def letter_stats(contents):
+    dic = {}
+    for letter in contents:
+        if letter.lower() not in dic:
+            dic[letter.lower()] = 1
+        else:
+            dic[letter.lower()] += 1
+    return dic
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dictionary(diccy):
+    result = []
+    for k, v in diccy.items():
+        result.append({"char": k, "num": v})
+    result.sort(reverse=True, key=sort_on)
+    return result
+
+def final_report(sorted):
+    for i in sorted:
+        if i["char"].isalpha():
+            print(f'{i["char"]}: {i["num"]}')
